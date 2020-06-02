364f0d361a231f5fde4ea53c44a0b4846627f767
Amy Cai <amycai89@gmail.com>
2020-06-01 22:37:27 -0700

    completed

--- desserts.py
+++ desserts.py
@@ -6,9 +6,32 @@ class Cupcake:
 
     def __repr__(self):
         """Human-readable printout for debugging."""
-
+        cache = {}
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
 
+
+    def __init__(self, name, flavor, price, qty): 
+      self.name = name 
+      self.flavor = flavor 
+      self.price = price 
+      self.qty = 0
+
+      self.cache[name] = self 
+
+    def add_stock(self, amount):
+       self.qty = self.qty + amount 
+
+    def sell(self, amount): 
+
+      if self.qty == 0: 
+        print('Sorry, these cupcakes are sold out')
+        return 
+
+      if self.qty < amount: 
+        self.qty = 0
+        return 
+
+      self.qty -= amount 
 
 if __name__ == '__main__':
     import doctest
